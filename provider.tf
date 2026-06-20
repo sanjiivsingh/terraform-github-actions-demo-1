64d41034a897591b095e16e676092b454a41d2ad
sanjiivsingh <sanjeevsinghred@gmail.com>
2026-06-21 01:28:46 +0530

    Added Azure remote backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.78.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstatesanjeev001"
+    container_name       = "tfstate"
+    key                  = "terraform-github-actions-demo.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
